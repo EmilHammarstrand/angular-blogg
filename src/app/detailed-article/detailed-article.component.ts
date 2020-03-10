b7a868dce45ae77861ff62a19e2fd184847a2500
Capaldi96 <capaldi_96@live.se>
2020-03-10 14:33:40 +0100

    added functions

--- src/app/detailed-article/detailed-article.component.ts
+++ src/app/detailed-article/detailed-article.component.ts
@@ -1,3 +1,4 @@
+import { DataServiceService } from './../data-service.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detailed-article.component.css']
 })
 export class DetailedArticleComponent implements OnInit {
+  latestArticle = this.DataService.getArticles('last');
 
-  constructor() { }
+  constructor(public DataService: DataServiceService) { }
 
   ngOnInit(): void {
   }
-
+  addArticle(title,content,author,estimatedTime){
+    this.DataService.addArticle(title,content,author,estimatedTime);
+    this.latestArticle = this.DataService.getArticles('last');
+  }
+  deleteArticle(){
+    this.DataService.removeArticle();
+    this.latestArticle = this.DataService.getArticles('last');
+  }
 }
